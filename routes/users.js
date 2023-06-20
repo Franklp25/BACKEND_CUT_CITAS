652e34395861b3d063e1916a3520b92a177120a9
Aqua-System <aquasystem.hidrocec.una.cr@gmail.com>
2023-06-20 10:45:44 -0600

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -9,15 +9,8 @@ import  {
     remove
 }  from '../controllers/UserController.js';
 
-// router.get('/list', userController.list);
-// router.get('/search/:data', userController.search);
-
-// router.post('/save', userController.save);
-// router.post('/update/:id', userController.update);
-// router.post('/delete/:id', userController.delete);
 router.route('/').get(list).post(save);
 router.route('/:data').get(search);
-
 router.route('/:id').put(update).delete(remove);
 
 export default router;
