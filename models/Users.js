65c4976e776494b83ef36eb66661a9c00d263ea1
Aqua-System <aquasystem.hidrocec.una.cr@gmail.com>
2023-10-03 23:15:03 -0600

    changes in users

--- models/Users.js
+++ models/Users.js
@@ -35,6 +35,10 @@ const UserSchema = new mongoose.Schema({
     role: {
         type: String,
         required: true
+    },
+    state: {
+        type: String,
+        required: true
     }
 });
 
